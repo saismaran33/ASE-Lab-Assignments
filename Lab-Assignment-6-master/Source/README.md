91ced8e98c373c3d89185d9c4bdf33b359acc413
saismaran33 <saismaran33@gmail.com>
2017-12-21 17:13:53 -0500

    Commit
    Updated

---
+++ Lab-Assignment-6-master/Source/README.md
@@ -0,0 +1,32 @@
+
+
+
+
+
+### Serve
+To serve in the browser
+
+```sh
+$ ionic serve
+```
+
+### To Add Platforms
+```sh
+$ cordova platform add android
+$ cordova platform add ios
+```
+
+### Run Platforms
+To run in avd
+
+```sh
+$ ionic run android
+$ ionic run ios
+```
+
+### Build Android APK File
+To build APK file to output folder
+
+```sh
+$ ionic build android
+```
---
+++ Lab-Assignment-6-master/Source/src/pages/home/home.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  weather:any;
+  location:{
+    city:string,
+    state:string
+  }
+
+  constructor(
+    public navCtrl: NavController, 
+    private weatherProvider:WeatherProvider,
+    private storage:Storage) {
+
+  }
+  calculateAndDisplayRoute() {
+    let that = this;
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer;
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
+    directionsDisplay.setMap(map);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+        that.MyLocation = new google.maps.LatLng(pos);
+
+      }, function() {
+
+      });
+    } else {
+      // Browser doesn't support Geolocation
+    }
+
+    directionsService.route({
+    origin: this.MyLocation,
+    destination: this.Destination,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+
+  ionViewWillEnter(){
+    this.storage.get('location').then((val) => {
+      if(val != null){
+        this.location = JSON.parse(val);
+      } else {
+        this.location = {
+          city: 'Miami',
+          state: 'FL'
+        }
+      }
+
+      this.weatherProvider.getWeather(this.location.city, this.location.state)  .subscribe(weather => {
+          this.weather = weather.current_observation;
+        });
+    });
+  }
+
+}
